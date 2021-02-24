5ffb40437969bcc43c2a934922f110f33e3dc8aa
Try Ajitiono <ballinst@gmail.com>
2021-02-24 18:26:51 +0700

    just need a node server (or what is seemingly a node server with mock, and adding cypress
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Card, ICardProps } from './components/Card';
 import './App.css';
 
@@ -31,50 +31,81 @@ const CARD_CONTENTS: ICardProps[] = [
 
 function App() {
   const [contents, setContents] = useState(CARD_CONTENTS);
+  const [form, setForm] = useState<Omit<ICardProps, 'date' | 'className'>>({
+    title: '',
+    body: ''
+  });
+
+  function handleChangeInput(
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const { name, value } = e.target;
+
+    setForm((oldForm) => ({
+      ...oldForm,
+      [name]: value
+    }));
+  }
 
   return (
     <div className="min-h-screen bg-indigo-600 p-4">
-      <form name="submit-rant" className="mb-16 w-full md:w-1/2">
-        <div>
-          <label
-            htmlFor="rant-title"
-            className="block text-sm font-bold text-indigo-100"
-          >
-            Rant title
-          </label>
-          <div className="mt-1 flex rounded-md shadow-sm">
-            <input
-              type="text"
-              name="rant-title"
-              id="rant-title"
-              className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
-            />
+      <div className="grid grid-cols-3 lg:grid-cols-4 gap-4 mb-16">
+        <form
+          name="submit-rant"
+          className="col-span-3 md:col-span-2 lg:col-span-3"
+        >
+          <div>
+            <label
+              htmlFor="title"
+              className="block text-sm font-bold text-indigo-100"
+            >
+              Rant title
+            </label>
+            <div className="mt-1 flex rounded-md shadow-sm">
+              <input
+                type="text"
+                name="title"
+                id="title"
+                onChange={handleChangeInput}
+                className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
+              />
+            </div>
           </div>
-        </div>
-        <div className="mt-2">
-          <label
-            htmlFor="rant-content"
-            className="block text-sm font-bold text-indigo-100"
-          >
-            Rant content
-          </label>
-          <div className="mt-1 flex rounded-md shadow-sm">
-            <textarea
-              name="rant-content"
-              id="rant-content"
-              className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
-            />
+          <div className="mt-2">
+            <label
+              htmlFor="body"
+              className="block text-sm font-bold text-indigo-100"
+            >
+              Rant content
+            </label>
+            <div className="mt-1 flex rounded-md shadow-sm">
+              <textarea
+                name="body"
+                id="body"
+                onChange={handleChangeInput}
+                className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"
+              />
+            </div>
           </div>
+          <div className="mt-4 text-right">
+            <button
+              type="submit"
+              className="w-24 inline-flex justify-center font-bold py-2 px-4 border border-transparent shadow-sm text-sm rounded-md text-indigo-500 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Post
+            </button>
+          </div>
+        </form>
+
+        <div className="hidden md:block pl-4 pr-2 pt-6 py-14">
+          <Card
+            title={form.title || 'Title placeholder'}
+            body={form.body || 'Body placeholder'}
+            date="February 24, 2021"
+            className="p-2 h-full"
+          />
         </div>
-        <div className="mt-4 text-right">
-          <button
-            type="submit"
-            className="w-24 inline-flex justify-center font-bold py-2 px-4 border border-transparent shadow-sm text-sm rounded-md text-indigo-500 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Post
-          </button>
-        </div>
-      </form>
+      </div>
 
       <section
         aria-label="Cards"
