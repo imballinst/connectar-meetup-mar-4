d08412438561e76b089de8c48fce5439a87647af
Try Ajitiono <ballinst@gmail.com>
2021-03-03 21:40:40 +0700

    test not broken but ui broken
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- frontend/src/components/Card.test.tsx
+++ frontend/src/components/Card.test.tsx
@@ -5,54 +5,40 @@ import '@testing-library/jest-dom/extend-expect';
 import { Card } from './Card';
 
 it('renders normal Card', () => {
-  const date = new Date(2021, 2, 4);
+  const date = 'Wed, 04 Mar 2021 00:00:00 GMT';
   const { getByText } = render(
-    <Card
-      title="Hello, world!"
-      date={date.toISOString()}
-      content="This is a test."
-    />
+    <Card title="Hello, world!" date={date} content="This is a test." />
   );
 
   expect(getByText('Hello, world!')).toBeInTheDocument();
   expect(getByText('This is a test.')).toBeInTheDocument();
-  expect(getByText('March 04, 2021 00:00')).toBeInTheDocument();
+  expect(getByText(date)).toBeInTheDocument();
 });
 
 describe('preview', () => {
   it('renders a preview Card with short text', () => {
-    const date = new Date(2021, 2, 4);
+    const date = 'Wed, 04 Mar 2021 00:00:00 GMT';
     const shortText = 'This is a test.';
 
     const { getByText } = render(
-      <Card
-        title="Hello, world!"
-        date={date.toISOString()}
-        content={shortText}
-        preview
-      />
+      <Card title="Hello, world!" date={date} content={shortText} preview />
     );
 
     expect(getByText('Hello, world!')).toBeInTheDocument();
     expect(getByText(shortText)).toBeInTheDocument();
-    expect(getByText('March 04, 2021 00:00')).toBeInTheDocument();
+    expect(getByText(date)).toBeInTheDocument();
   });
 
   it('renders a preview Card with short text', () => {
-    const date = new Date(2021, 2, 4);
+    const date = 'Wed, 04 Mar 2021 00:00:00 GMT';
     const superLongText = Array.from(new Array(290), () => 'a').join('');
 
     const { getByText } = render(
-      <Card
-        title="Hello, world!"
-        date={date.toISOString()}
-        content={superLongText}
-        preview
-      />
+      <Card title="Hello, world!" date={date} content={superLongText} preview />
     );
 
     expect(getByText('Hello, world!')).toBeInTheDocument();
     expect(getByText(`${superLongText.slice(0, 280)}...`)).toBeInTheDocument();
-    expect(getByText('March 04, 2021 00:00')).toBeInTheDocument();
+    expect(getByText(date)).toBeInTheDocument();
   });
 });
